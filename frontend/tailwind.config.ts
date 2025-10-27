696e397bcf00201ca71451cf0d173c86b85254bc
fazafa <karipik.bakso@gmail.com>
2025-10-27 14:30:14 +0700

    Add custom color palette to Tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,1 +1,15 @@
-
+module.exports = {
+content: ["./src/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/app/**/*.{ts,tsx}"],
+theme: {
+extend: {
+colors: {
+sirama: {
+900: '#002916',
+800: '#004225',
+700: '#0a6b43'
+}
+}
+}
+},
+plugins: []
+}
