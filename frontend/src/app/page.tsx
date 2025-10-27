e82807c75f3c01664efd8f62aa3542331d272bc3
fazafa <karipik.bakso@gmail.com>
2025-10-27 16:31:48 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { fetchMock } from '../lib/api'
+import { fetchMock } from 'src/lib/api'
 
 export default function Page(){
 const [stats, setStats] = useState<any>(null)
