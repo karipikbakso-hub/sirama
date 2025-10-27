577f2beed006d7019e65fd3e574acf8056826c4a
fazafa <karipik.bakso@gmail.com>
2025-10-27 18:26:18 +0700

    Update page.tsx

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -3,7 +3,7 @@ import SettingsPage from '../../features/settings'
 
 export default function Page() {
   return (
-    <ProtectedLayout>
+    <ProtectedLayout allowedRoles={['admin']}>
       <SettingsPage />
     </ProtectedLayout>
   )
